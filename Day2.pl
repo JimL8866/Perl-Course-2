2fb2726526600c5bf9478696db6b8d8fbfda1f7a
Jim <chengxuyuan9527@gmail.com>
2022-05-13 08:33:09 +1000

    practice

--- Day2.pl
+++ Day2.pl
@@ -1,15 +1,31 @@
+# use strict;
+# use warnings;
+# use 5.10.0;
+
+# my $file_name = 'C:\Users\Owner\OneDrive\Documents\GitHub\Perl Course 2\LIKA0078.txt';
+# my $file_output = 'C:\Users\Owner\OneDrive\Documents\GitHub\Perl Course 2\LIKA0078_replace.txt';
+# open INPUT, $file_name;
+# open OUTPUT, ">", $file_output;
+# while (my $line=<INPUT>){
+	# chomp $line;
+	# $line =~ s/\bELECTRICITY\b/WATER/;
+	# say OUTPUT $line;
+# }
+# close INPUT;
+# close OUTPUT;
 use strict;
 use warnings;
-use 5.10.0;
 
-my $file_name = 'C:\Users\Owner\OneDrive\Documents\GitHub\Perl Course 2\LIKA0078.txt';
-my $file_output = 'C:\Users\Owner\OneDrive\Documents\GitHub\Perl Course 2\LIKA0078_replace.txt';
-open INPUT, $file_name;
-open OUTPUT, ">", $file_output;
-while (my $line=<INPUT>){
-	chomp $line;
-	$line =~ s/\bELECTRICITY\b/WATER/;
-	say OUTPUT $line;
+sub main {
+	my @list = @_;
+	foreach my $item(@list) {
+		if ($item =~ /\w+@\w+.\w+/) {
+			print "Email address $item is valid\n";
+		}else {
+			print "Email address $item is not valid\n";
+		}
+	}
 }
-close INPUT;
-close OUTPUT;
+
+my @list = qw (liucy035@gamil.com xxxx.com eeefdsfdsfdsd);
+main(@list);
